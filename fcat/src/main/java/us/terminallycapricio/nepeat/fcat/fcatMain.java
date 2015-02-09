b07e7d94362a481f761239f7270ca2a449fe422e
nepeat <nepeat@users.noreply.github.com>
2015-02-08 21:30:58 -0900

    Initial commit

---
+++ fcat/src/main/java/us/terminallycapricio/nepeat/fcat/fcatMain.java
@@ -0,0 +1,44 @@
+package us.terminallycapricio.nepeat.fcat;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+public final class fcatMain extends JavaPlugin implements Listener {
+	
+	public static void registerEvents(Plugin plugin, Listener... listeners) {
+		for (Listener listener : listeners) {
+			plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+		}
+	}
+	
+    @Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    	getLogger().info("onEnable has been invoked!");
+    	
+    	// Event hooks
+    	registerEvents(this, new EventListener(this));
+    	getServer().getPluginManager().registerEvents(this, this);
+    	
+    	// Command hooks
+    	this.getCommand("killtnt").setExecutor(new CommandHandler(this));
+    	this.getCommand("getchance").setExecutor(new CommandHandler(this));
+    	this.getCommand("setchance").setExecutor(new CommandHandler(this));
+
+    	// Config
+    	loadConfig();
+    	getServer().broadcastMessage(ChatColor.RED + "[fcat:WARNING] BUTTFUCKERY PLUGIN ACTIVE.");
+    }
+ 
+    public void loadConfig() {
+    	this.saveDefaultConfig();
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    	getLogger().info("onDisable has been invoked!");
+    }
+
+}
