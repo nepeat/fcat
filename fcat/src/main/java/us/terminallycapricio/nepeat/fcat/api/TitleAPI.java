131b50d1f43fe76f51b86b6f4285d6da710d28ad
nepeat <nepeat@users.noreply.github.com>
2016-03-13 16:20:43 -0800

    Update plugin for 1.9 latest.

--- fcat/src/main/java/us/terminallycapricio/nepeat/fcat/api/TitleAPI.java
+++ fcat/src/main/java/us/terminallycapricio/nepeat/fcat/api/TitleAPI.java
@@ -1,12 +1,12 @@
 package us.terminallycapricio.nepeat.fcat.api;
 
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
-import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
-import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
-import net.minecraft.server.v1_8_R3.PacketPlayOutTitle.EnumTitleAction;
+import net.minecraft.server.v1_9_R1.IChatBaseComponent;
+import net.minecraft.server.v1_9_R1.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_9_R1.PacketPlayOutTitle;
+import net.minecraft.server.v1_9_R1.PacketPlayOutTitle.EnumTitleAction;
 
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.json.simple.JSONObject;
 
